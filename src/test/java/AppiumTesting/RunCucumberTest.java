c135b63c2d6b8f9f9322f4bdba528f453fb6bcc7
Furkan Peg <frknpg@gmail.com>
2019-11-08 01:09:33 +0300

    first commit

---
+++ src/test/java/AppiumTesting/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package AppiumTesting;
+
+
+import io.cucumber.java.Before;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/AppiumTesting")
+public class RunCucumberTest {
+
+    @Before
+    public void startAppium() {
+
+    }
+}
